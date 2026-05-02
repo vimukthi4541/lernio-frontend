ee0119be90b64d1f9e6d8d43b2a6f4233e41e965
vimukthi4541 <it24610782@my.sliit.lk>
2026-05-02 11:25:55 +0530

    Finalized UI and toast notifications for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
     <Router>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         
         <Route path="/" element={<Navigate to="/login" />} />
